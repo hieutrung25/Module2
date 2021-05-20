59fbd454d253cc5eadbdb731199b14d6c4d46b07
hieutrung25 <hieunt25051999@gmail.com>
2021-05-20 14:22:29 +0700

    Module2

--- MD2-SS11-Stack,Queue/Trien_Khai_Queue_List_Lien_Ket_Vong/src/Main.java
+++ MD2-SS11-Stack,Queue/Trien_Khai_Queue_List_Lien_Ket_Vong/src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+    public static void main(String[] args) {
+        MyQueue.Queue queue = new MyQueue.Queue();
+        queue.front = queue.rear = null;
+        MyQueue.enQueue(queue, 5);
+        MyQueue.enQueue(queue, 10);
+        MyQueue.enQueue(queue, 25);
+
+        MyQueue.displayQueue(queue);
+
+        System.out.print("\nXóa giá trị: " + MyQueue.deQueue(queue));
+        System.out.print("\nXóa giá trị: " + MyQueue.deQueue(queue) + "\n");
+
+        MyQueue.displayQueue(queue);
+
+        MyQueue.enQueue(queue, 30);
+        MyQueue.enQueue(queue, 35);
+
+        System.out.println();
+        MyQueue.displayQueue(queue);
+    }
 }
